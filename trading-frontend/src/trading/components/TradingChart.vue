99cd7cdf5652ea820ea2be9d8ad261e413d8cdb7
Carlos_Silvestri <carlosarmando99@hotmail.com>
2023-02-06 15:43:31 -0400

    FEAT Real time data bitcoind to usdt and Responsive trading table

--- trading-frontend/src/trading/components/TradingChart.vue
+++ trading-frontend/src/trading/components/TradingChart.vue
@@ -1,30 +1,117 @@
 <template>
-  <trading-vue class="m-auto" :data="$data" :width="width" :height="height">
+  <trading-vue
+    class="m-auto"
+    :data="chart"
+    :width="width"
+    :height="height"
+    ref="tvjs"
+  >
   </trading-vue>
 </template>
 <script>
-import TradingVue from "trading-vue-js";
-import tradingService from "@/trading/services/trading.js";
-import { convertToArray } from "@/trading/utils/utils.js";
+import TradingVue, { Constants, DataCube, Utils } from "trading-vue-js";
+import { Stream, format, parse_binance } from "@/trading/utils/utils.js";
+
 export default {
-  name: "app",
+  name: "TradingChart",
   components: { TradingVue },
   data() {
     return {
+      chart: {},
       width: window.innerWidth - 370,
       height: window.innerHeight - 100,
       ohlcv: [],
+      WSS: `${process.env.VUE_APP_WSS_BINANCE}/btcusdt@aggTrade`,
+      URL: `${process.env.VUE_APP_API_BINANCE}/api/v3/klines?symbol=`,
     };
   },
   methods: {
-    async getLines() {
-      const data = await tradingService.get();
-      const lines = convertToArray(data);
-      this.ohlcv = JSON.parse(JSON.stringify(lines));
+    /**
+     * Load lines and handle them.
+     * @returns void.
+     */
+    async load_lines() {
+      // Load the last data chunk & init DataCube:
+      let now = Utils.now();
+      const data = await this.get_lines([now - Constants.HOUR4, now]);
+      this.handle_get_lines(data);
     },
+    /**
+     * Get lines
+     * @param range : [timestamp, timestamp]
+     * @returns Object : { "chart.data": number[]  }
+     */
+    async get_lines(range) {
+      let [t1, t2] = range;
+      let x = "BTCUSDT";
+      let q = `${x}&interval=1m&startTime=${t1}&endTime=${t2}`;
+      let endPoint = `${this.URL}${q}`;
+
+      let r = await fetch(endPoint).then((r) => r.json());
+      return format(parse_binance(r));
+    },
+    on_trades(trade) {
+      this.chart.update({
+        t: trade.T, // Exchange time (optional)
+        price: parseFloat(trade.p), // Trade price
+        volume: parseFloat(trade.q), // Trade amount
+        "datasets.binance-btcusdt": [
+          // Update dataset
+          trade.T,
+          trade.m ? 0 : 1, // Sell or Buy
+          parseFloat(trade.q),
+          parseFloat(trade.p),
+        ],
+        // ... other onchart/offchart updates
+      });
+    },
+    /**
+     * Init DataCube:
+     * @param data: Object { "chart.data": number[]  }
+     */
+    handle_get_lines(data) {
+      this.chart = new DataCube(
+        {
+          ohlcv: data["chart.data"],
+        },
+        { aggregation: 100 }
+      );
+      // Register a stream of trades
+      this.$refs.tvjs.resetChart();
+      this.stream = new Stream(this.WSS);
+      this.stream.ontrades = this.on_trades;
+    },
+    /**
+     * Create resize event and calls on_resize.
+     * @returns void
+     */
+    resize_event() {
+      window.addEventListener("resize", this.on_resize);
+      this.on_resize();
+    },
+    /**
+     * Destroy resize event.
+     */
+    destroy_resize_event() {
+      window.removeEventListener("resize", this.on_resize);
+      if (this.stream) this.stream.off();
+    },
+    /**
+     * Adapt screen to be responsive.
+     * @returns void
+     */
+    on_resize() {
+      this.width = window.innerWidth - 370;
+      this.height = window.innerHeight - 100;
+    },
+    // Functions to load when the component is mounted.
     async init() {
-      await Promise.all([this.getLines()]);
+      this.resize_event();
+      await Promise.all([this.load_lines()]);
     },
+  },
+  beforeDestroy() {
+    this.destroy_resize_event();
   },
   async mounted() {
     await this.init();
--- trading-frontend/src/trading/utils/utils.js
+++ trading-frontend/src/trading/utils/utils.js
@@ -1,19 +1,55 @@
 /**
- * Convert to array, so can be used on tradingcomponent.
- * @param payload: Kline[]
- * @returns [ string[] | number[] ]
+ * Connect to web sockets.
+ * @param url: string
+ * @returns Object
  */
-export const convertToArray = (payload) => {
-  if (!payload) return [];
-  const data = payload.map((item) => {
-    return [
-      item.timestamp,
-      Number(item.open),
-      Number(item.high),
-      Number(item.low),
-      Number(item.close),
-      Number(item.volume),
-    ];
+export const Stream = (url) => {
+  var ws = new WebSocket(url);
+  var cb = () => {};
+
+  ws.onopen = function () {
+    console.log("Websocket is opened");
+  };
+
+  ws.onmessage = function (data) {
+    try {
+      data = JSON.parse(data.data);
+      cb(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return {
+    set ontrades(val) {
+      cb = val;
+    },
+    off() {
+      ws.close(1000);
+    },
+  };
+};
+/**
+ * Format data.
+ * @param data: Array
+ * @returns Object
+ */
+export const format = (data) => {
+  // Each query sets data to a corresponding overlay
+  return {
+    "chart.data": data,
+    // other onchart/offchart overlays can be added here,
+    // but we are using Script Engine to calculate some:
+    // see EMAx6 & BuySellBalance
+  };
+};
+// Parse a specific exchange format
+export const parse_binance = (data) => {
+  if (!Array.isArray(data)) return [];
+  return data.map((x) => {
+    for (var i = 0; i < x.length; i++) {
+      x[i] = parseFloat(x[i]);
+    }
+    return x.slice(0, 6);
   });
-  return data;
 };
